4ab193c65fd4e17da91c766be5a8eb44124afd88
Jonathan Lopez <jonathanglopez@gmail.com>
2020-02-15 16:11:30 -0500

    Passed first test

--- space-age/space_age.cpp
+++ space-age/space_age.cpp
@@ -1,5 +1,8 @@
 #include "space_age.h"
 
 namespace space_age {
+space_age::space_age(int seconds_undef) { seconds_undefined = seconds_undef; }
+
+int space_age::seconds() const { return seconds_undefined; }
 
 }  // namespace space_age
--- space-age/space_age.h
+++ space-age/space_age.h
@@ -2,7 +2,15 @@
 #define SPACE_AGE_H
 
 namespace space_age {
+class space_age {
+ private:
+  int seconds_undefined;
+
+ public:
+  explicit space_age(int seconds_undef);
+  int seconds() const;
+};
 
 }  // namespace space_age
 
-#endif // SPACE_AGE_H
+#endif  // SPACE_AGE_H
