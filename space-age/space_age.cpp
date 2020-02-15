ed240b14f90c8e32dc918a3343d3ff15f6b16826
Jonathan Lopez <jonathanglopez@gmail.com>
2020-02-15 16:39:04 -0500

    Passed test 2

--- space-age/space_age.cpp
+++ space-age/space_age.cpp
@@ -1,8 +1,14 @@
 #include "space_age.h"
 
 namespace space_age {
-space_age::space_age(int seconds_undef) { seconds_undefined = seconds_undef; }
+space_age::space_age(int input_seconds) {
+  space_age::age_in_seconds = input_seconds;
+  space_age::age_in_years = age_in_seconds / seconds_in_year;
+}
 
-int space_age::seconds() const { return seconds_undefined; }
+int space_age::seconds() const { return age_in_seconds; }
+double space_age::on_earth() const {
+  return age_in_years * year_convert.at("earth");
+}
 
 }  // namespace space_age
--- space-age/space_age.h
+++ space-age/space_age.h
@@ -1,14 +1,23 @@
 #if !defined(SPACE_AGE_H)
 #define SPACE_AGE_H
+#include <map>
+#include <string>
 
 namespace space_age {
 class space_age {
  private:
-  int seconds_undefined;
+  int age_in_seconds;
+  double seconds_in_year = 31557600.0;
+  double age_in_years;
+  std::map<std::string, double> year_convert = {
+      {"mercury", 0.2408467}, {"venus", 0.61519726},  {"earth", 1.0},
+      {"mars", 1.8808158},    {"jupiter", 11.862615}, {"saturn", 29.447498},
+      {"uranus", 84.016846},  {"neptune", 164.79132}};
 
  public:
-  explicit space_age(int seconds_undef);
+  explicit space_age(int input_seconds);
   int seconds() const;
+  double on_earth() const;
 };
 
 }  // namespace space_age
